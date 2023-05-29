5819f97c1ccce7e59dcab0e27c964c67f513d410
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-05-29 09:40:02 -0700

    WIP set cookie with cookie-parser, redirect to / to simulate session

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -25,6 +26,7 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use(cookieParser());
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -32,6 +34,7 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const eventsRoutes = require('./routes/events');
+const loginRoutes = require('./routes/login');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,6 +43,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/api/events', eventsRoutes);
+app.use('/login/:user_id', loginRoutes)
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -48,6 +52,7 @@ app.use('/api/events', eventsRoutes);
 
 app.get('/', (req, res) => {
   res.render('index');
+  console.log('Cookies: ', req.cookies);
 });
 
 app.listen(PORT, () => {
